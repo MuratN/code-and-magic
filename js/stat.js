d08710c3767c8109c239f6efdcbbd2566b1666f2
Nyrov Myrat <mnyrov@list.ru>
2021-02-16 14:31:21 +0300

    histogram corrected2

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,17 @@
-window.renderStatistics = function (ctx, names, times) {
+const drawCloud = (ctx) => {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
     ctx.fillRect(110, 20, 420, 270);
-    ctx.fillStyle = 'rgba(256, 256, 256, 1)';
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
     ctx.strokeRect(100, 10, 420, 270);
     ctx.fillRect(100, 10, 420, 270);
     ctx.font = '16px PT Mono';
     ctx.textBaseline = 'hanging';
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText('Ура вы победили!', 120, 40);
-    ctx.fillText('Список результатов:', 120, 60);
+    ctx.fillText('Ура вы победили!', 120, 30);
+    ctx.fillText('Список результатов:', 120, 50);
+}
 
+const getMaxValue = (times) => {
     var max = -1;
     var maxIndex = -1;
 
@@ -20,27 +22,30 @@ window.renderStatistics = function (ctx, names, times) {
             maxIndex = i;
         }
     }
+    return max;
+}
 
-    var histogramHeight = -100;
-    var step = histogramHeight / (max - 0);
+window.renderStatistics = function (ctx, names, times) {
+    drawCloud(ctx);
+    var max = getMaxValue(times);
+
+    var histogramHeight = 150;
+    var ratio = histogramHeight / max;
     var barWidth = 40;
-    var indent = 80;
+    var indent = 90;
     var initialY = 240;
     var initialX = 140;
-    var lineHeight = 15;
-    var histogramMargin = 110;
-    var timesMargin = 20;
     
     for(var i = 0; i < times.length; i++) {
         ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-        ctx.fillText(names[i], initialX + indent * i, initialY + histogramHeight + histogramMargin);
-        ctx.fillText(Math.floor(times[i]), initialX + indent * i, Math.floor(times[i] * step) - 2 * histogramHeight + timesMargin);
+        ctx.fillText(names[i], initialX + indent * i, initialY + 10);
+        ctx.fillText(Math.floor(times[i]), initialX + indent * i, initialY - Math.floor(times[i] * ratio) - 20);
         if (names[i] === 'Вы') {
             ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-            ctx.fillRect(initialX + indent * i, initialY, barWidth , times[i] * step);
+            ctx.fillRect(initialX + indent * i, initialY, barWidth , -times[i] * ratio);
         } else {
             ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
-            ctx.fillRect(initialX + indent * i, initialY, barWidth , times[i] * step);
+            ctx.fillRect(initialX + indent * i, initialY, barWidth , -times[i] * ratio);
         }
     }
 }
